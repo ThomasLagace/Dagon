af9f07ee4e208c208e72db8de1b9f21f64e1a90e
Thomas Lagace <ThomasLagace@users.noreply.github.com>
2017-11-17 13:20:47 -0500

    Added syntax highlighting for code

--- web/assets/templates/head.php
+++ web/assets/templates/head.php
@@ -3,4 +3,8 @@
     <head>
         <title>Thomas Lagace's Blog</title>
         <link rel="stylesheet" type="text/css" href="/assets/css/style.css">
+        <link rel="stylesheet"
+              href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/default.min.css">
+        <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+        <script>hljs.initHighlightingOnLoad();</script>
     </head>
